f34ce39ed06177bb84c122955e7234c6ef7f2e77
Petr Janik <peta.janik@email.cz>
2022-09-25 13:16:36 +0200

    Add daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <button className="btn">Hello daisyUI</button>
   );
 }
 
