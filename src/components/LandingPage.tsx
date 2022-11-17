e6e7a3497ddab0d8c36bc2044f04ba2d2d050449
Petr Janik <peta.janik@email.cz>
2022-11-17 21:27:58 +0100

    Inline logo svg for faster and CLS-free load

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../icons/weather.svg';
+import {ReactComponent as Logo} from '../icons/weather.svg';
 import LoginButton from './LoginButton';
 
 const LandingPage = () => {
@@ -7,7 +7,7 @@ const LandingPage = () => {
     <div className="hero min-h-screen bg-top" style={{ backgroundImage: 'url("/landing_page_background.avif")'}}>
       <div className="hero-content text-center text-neutral-content -mt-32" >
         <div className="max-w-lg">
-          <img src={logo} className="w-1/2 h-1/2 mx-auto" alt="sun partly hidden behind a cloud" />
+          <Logo className="w-1/2 h-1/2 mx-auto"/>
           <h1 className="mb-5 text-6xl font-bold">Weather Advisor</h1>
           <p className="mb-5">Plan the vacation of your dreams! We’ll give you the best locations to perform an activity of your choice considering the optimal weather conditions.</p>
           <LoginButton/>
