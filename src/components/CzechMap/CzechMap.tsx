2f9ed2434eca68a3bf508a0a9698b8db1f725733
Jan.Solar <Jan.Solar@adacta-fintech.com>
2022-11-21 10:53:18 +0100

    Add select-none, revert normalization

--- src/components/CzechMap/CzechMap.tsx
+++ src/components/CzechMap/CzechMap.tsx
@@ -95,7 +95,8 @@ const CzechMap: FC<Props> = ({
             backgroundColor: 'rgba(255, 255, 255, 0.5)',
           }}></div>
         <p className={'text-center text-3xl ' +
-                    'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5/6'}
+                    'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5/6 ' +
+                    'select-none'}
         >{hintText}</p>
       </>)}
   </div>;
